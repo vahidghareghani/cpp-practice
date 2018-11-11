ff59215b8fb7e5329dc81f3cfeee1d18ffe41281
vahid ghareghani <vahidgh.vgh@gmail.com>
2018-11-11 23:52:18 +0330

    edit ShapeList.h and add method sort

--- ShapeList.h
+++ ShapeList.h
@@ -9,8 +9,25 @@ class ShapeList {
     vector<Shape*> shapes;
     
 public:
+    struct {
+        bool operator()(Shape* a, Shape* b) const
+        {
+            return a->getArea() < b->getArea();
+        }
+    } Cmp;
     void addShape(Shape& t){
         shapes.push_back(&t);
+    }
+    unsigned long size(){
+        return shapes.size();
+    }
+    void print(){
+        for (auto it=shapes.begin(); it!=shapes.end(); it++) {
+            (*it)->draw();
+        }
+    }
+    void sort(){
+        ::sort(shapes.begin(), shapes.end(), Cmp);
     }
 };
 
